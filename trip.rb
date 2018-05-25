2857ca80687632c04282cd19da5ff6ae0e3c925f
Justine Koa <justinekoa@gmail.com>
2018-05-25 01:46:32 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -4,8 +4,8 @@ stay = gets.chomp.capitalize
 puts "What sites would you like to visit?"
 sites = gets.chomp
 puts "What food would you like to eat?"
-foods = gets.chomp
+eat = gets.chomp
 puts "How many nights would you like to stay?"
 num_nights = gets.chomp
 
-puts "You want to go to #{stay} to visit #{sites}. The foods you would like to eat are #{foods}. Your trip will last #{num_nights} nights."
+puts "You want to go to #{stay} to visit #{sites}. The foods you would like to eat are #{eat}. Your trip will last #{num_nights} nights."
