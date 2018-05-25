fd9e2bf84e0e1c55fc46fb8c28a5cbf86596846e
Justine Koa <justinekoa@gmail.com>
2018-05-25 01:44:43 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,1 +1,11 @@
 # Code your prompts here!
+puts "Where would you like to stay?"
+place = gets.chomp
+puts "What sites would you like to visit?"
+sites = gets.chomp
+puts "What food would you like to eat?"
+foods = gets.chomp
+puts "How many nights would you like to stay?"
+num_nights = gets.chomp
+
+puts "You want to go to #{place} to visit #{sites}. The foods you would like to eat are #{foods}. Your trip will last #{num_nights} nights."
